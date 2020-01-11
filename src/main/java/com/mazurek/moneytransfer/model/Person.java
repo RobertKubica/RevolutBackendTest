96843ab3bc2f340b88b1b34b95758cf921c0884f
Kuba <jmazurek256@gmail.com>
2020-01-11 15:56:25 +0100

    Add Person factory method

--- src/main/java/com/mazurek/moneytransfer/model/Person.java
+++ src/main/java/com/mazurek/moneytransfer/model/Person.java
@@ -1,4 +1,43 @@
 package com.mazurek.moneytransfer.model;
 
+import com.google.common.base.Preconditions;
+
+import java.util.regex.Pattern;
+
 public class Person {
+
+    private final String name;
+    private final String phoneNumber;
+
+    private Person(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public static Person create(String name, String phoneNumber){
+        validateName(name);
+        validatePhoneNumber(phoneNumber);
+        return new Person(name, phoneNumber);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    private static void validateName(String name) {
+        Preconditions.checkNotNull(name, "Person's name cannot be null");
+        Pattern pattern = Pattern.compile("[A-Za-z]+( [A-Za-z]+)?");
+        Preconditions.checkArgument(pattern.matcher(name).matches(), String.format("%s is not correct name", name));
+    }
+
+    private static void validatePhoneNumber(String phoneNumber) {
+        Preconditions.checkNotNull(phoneNumber, "Person's phone number cannot be null");
+        Pattern pattern = Pattern.compile("(\\+?\\d{2})?\\d{8,9}");
+        Preconditions.checkArgument(pattern.matcher(phoneNumber).matches(), String.format("%s is not correct phone number", phoneNumber));
+
+    }
 }
---
+++ src/test/java/com/mazurek/moneytransfer/model/PersonTest.java
@@ -0,0 +1,66 @@
+package com.mazurek.moneytransfer.model;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PersonTest {
+
+
+    @DataProvider
+    public static Object[][] correctData() {
+        return new Object[][]{
+                {"John Smith", "+48123456789"},
+                {"John Smith", "48123456789"},
+                {"John Smith", "123456789"},
+                {"John", "+48123456789"},
+        };
+    }
+
+    @DataProvider
+    public static Object[][] incorrectNames() {
+        return new Object[][]{
+                {"John_Smith"},
+                {"J0hn"},
+                {""},
+                {"John."}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] incorrectPhoneNumbers() {
+        return new Object[][]{
+                {"1 2"},
+                {"+48 123 456 789"},
+                {"++48123456789"},
+                {"+48I23456789"},
+                {""},
+        };
+    }
+
+    @Test(dataProvider = "correctData")
+    public void shouldCreatePersonWhenCorrectDataIsProvided(String name, String phoneNumber) {
+        Person person = Person.create(name, phoneNumber);
+        assertThat(person)
+                .extracting(Person::getName, Person::getPhoneNumber)
+                .containsExactly(name, phoneNumber);
+    }
+
+    @Test(dataProvider = "incorrectNames")
+    public void shouldThrowIllegalArgumentExceptionOnIncorrectName(String name){
+        String correctPhoneNumber = "+48123456789";
+        assertThatThrownBy(() -> Person.create(name, correctPhoneNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("%s is not correct name", name));
+    }
+
+    @Test(dataProvider = "incorrectPhoneNumbers")
+    public void shouldThrowIllegalArgumentExceptionOnIncorrectPhoneNumbers(String phoneNumber){
+        String correctName = "John Smith";
+        assertThatThrownBy(() -> Person.create(correctName, phoneNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(String.format("%s is not correct phone number", phoneNumber));
+    }
+}
