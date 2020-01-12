65998398b37e572cdf3436089270b9720a4f919b
Kuba <jmazurek256@gmail.com>
2020-01-12 16:01:35 +0100

    Add simple methods of money transfer

--- src/main/java/com/mazurek/moneytransfer/MoneyTransferController.java
+++ src/main/java/com/mazurek/moneytransfer/MoneyTransferController.java
@@ -1,8 +1,10 @@
 package com.mazurek.moneytransfer;
 
+import com.google.common.base.Preconditions;
 import com.mazurek.moneytransfer.model.Account;
 import com.mazurek.moneytransfer.model.Person;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -28,6 +30,26 @@ public class MoneyTransferController {
 
     public Optional<Account> getAccountById(String id) {
         return Optional.ofNullable(accounts.get(id));
+    }
+
+    public void deposit(String id, BigDecimal amount){
+        Preconditions.checkArgument(amount.compareTo(BigDecimal.ZERO) > 0, String.format("Amount (%s) must be positive", amount));
+        Account account = accounts.get(id);
+        account.setBalance(account.getBalance().add(amount));
+    }
+
+    public void withdraw(String id, BigDecimal amount){
+        Preconditions.checkArgument(amount.compareTo(BigDecimal.ZERO) > 0, String.format("Amount (%s) must be positive", amount));
+        Account account = accounts.get(id);
+        if(account.getBalance().compareTo(amount)<0){
+            throw new IllegalArgumentException(String.format("Balance is too low to withdraw: %s", amount));
+        }
+        account.setBalance(account.getBalance().subtract(amount));
+    }
+
+    public void transfer(String sourceId, String targetId, BigDecimal amount) {
+        withdraw(sourceId,amount);
+        deposit(targetId,amount);
     }
 
     private String createNewId() {
