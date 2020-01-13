20a6ccf8d862dd40faf4e2372b8732d62e661291
Kuba <jmazurek256@gmail.com>
2020-01-13 19:25:50 +0100

    Remove redundant setters

--- src/main/java/com/mazurek/moneytransfer/rest/requests/TransferRequest.java
+++ src/main/java/com/mazurek/moneytransfer/rest/requests/TransferRequest.java
@@ -3,7 +3,6 @@ package com.mazurek.moneytransfer.rest.requests;
 import java.math.BigDecimal;
 
 public class TransferRequest {
-
     private String sourceAccountId;
     private String targetAccountId;
     private BigDecimal amount;
@@ -12,23 +11,11 @@ public class TransferRequest {
         return sourceAccountId;
     }
 
-    public void setSourceAccountId(String sourceAccountId) {
-        this.sourceAccountId = sourceAccountId;
-    }
-
     public String getTargetAccountId() {
         return targetAccountId;
     }
 
-    public void setTargetAccountId(String targetAccountId) {
-        this.targetAccountId = targetAccountId;
-    }
-
     public BigDecimal getAmount() {
         return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
     }
 }
