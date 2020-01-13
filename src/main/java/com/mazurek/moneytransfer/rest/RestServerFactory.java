f1114ba3bc42a9330112adc9e73a50936862d5e5
Kuba <jmazurek256@gmail.com>
2020-01-13 18:00:24 +0100

    Add methods to rest api

--- src/main/java/com/mazurek/moneytransfer/rest/RestServerFactory.java
+++ src/main/java/com/mazurek/moneytransfer/rest/RestServerFactory.java
@@ -14,9 +14,12 @@ public class RestServerFactory {
 
     public Undertow createServer() {
         RoutingHandler routingHandler = Handlers.routing()
+                .add("GET", "/", ex -> ex.getResponseSender().send("OK"))
                 .add("POST", "/account", new CreateAccountHandler(controller))
                 .add("GET", "/account/{id}", new GetAccountInfoHandler(controller))
-                .add("POST", "/deposit", new DepositHandler(controller));
+                .add("POST", "/deposit", new DepositHandler(controller))
+                .add("POST", "/withdraw", new WithdrawHandler(controller))
+                .add("POST", "/transfer", new TransferHandler(controller));
         return Undertow.builder()
                 .addHttpListener(8080, "localhost")
                 .setHandler(routingHandler).build();
